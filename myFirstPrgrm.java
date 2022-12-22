4192958aa768af534273df1ce3f153ca130e7f7b
Admin <niru.nayak2009@gmail.com>
2022-12-22 17:37:29 +0000

    updated

--- myFirstPrgrm.java
+++ myFirstPrgrm.java
@@ -3,7 +3,7 @@ package Com.PlaywithNumbers;
 public class myFirstPrgrm {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.print("hello world");
 
 	}
 
